ca2e669ca051ae9c511af6766e489c03512bbbbc
developer-abdulali <ab.ali.soomro@gmail.com>
2023-07-14 01:44:19 +0500

    added changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavComp from "./components/NavComp";
+import Slider from "./components/Slider";
 import UserDiv from "./components/UserDiv";
 
 const App = ()=> {
   return (
     <div className="app-div">
-      < NavComp />
-      < UserDiv />
+      <NavComp />
+      <UserDiv />
+      <Slider />
     </div>
   );
 }
